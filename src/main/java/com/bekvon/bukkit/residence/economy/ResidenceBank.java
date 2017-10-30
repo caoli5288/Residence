f04657752f4531e1e61d12090a5af36f4df35597
caoli5288 <caoli5288@gmail.com>
2017-10-30 16:55:20 +0800

    Mig to maven and cleanup

---
+++ src/main/java/com/bekvon/bukkit/residence/economy/ResidenceBank.java
@@ -0,0 +1,87 @@
+package com.bekvon.bukkit.residence.economy;
+
+import org.bukkit.command.CommandSender;
+
+import com.bekvon.bukkit.residence.Residence;
+import com.bekvon.bukkit.residence.containers.Flags;
+import com.bekvon.bukkit.residence.containers.lm;
+import com.bekvon.bukkit.residence.protection.ClaimedResidence;
+import com.bekvon.bukkit.residence.protection.FlagPermissions.FlagCombo;
+
+import org.bukkit.entity.Player;
+
+public class ResidenceBank {
+    int storedMoney;
+    ClaimedResidence res;
+
+    public ResidenceBank(ClaimedResidence parent) {
+	storedMoney = 0;
+	res = parent;
+    }
+
+    public int getStoredMoney() {
+	return storedMoney;
+    }
+
+    public void setStoredMoney(int amount) {
+	storedMoney = amount;
+    }
+
+    public void add(int amount) {
+	storedMoney = storedMoney + amount;
+    }
+
+    public boolean hasEnough(int amount) {
+	if (storedMoney >= amount)
+	    return true;
+	return false;
+    }
+
+    public void subtract(int amount) {
+	storedMoney = storedMoney - amount;
+	if (storedMoney < 0)
+	    storedMoney = 0;
+    }
+
+    public void withdraw(CommandSender sender, int amount, boolean resadmin) {
+	if (!(sender instanceof Player))
+	    return;
+	Player player = (Player) sender;
+	if (!Residence.getInstance().getConfigManager().enableEconomy()) {
+	    Residence.getInstance().msg(sender, lm.Economy_MarketDisabled);
+	}
+	if (!resadmin && !res.getPermissions().playerHas(player, Flags.bank, FlagCombo.OnlyTrue)) {
+	    Residence.getInstance().msg(sender, lm.Bank_NoAccess);
+	    return;
+	}
+	if (!hasEnough(amount)) {
+	    Residence.getInstance().msg(sender, lm.Bank_NoMoney);
+	    return;
+	}
+	if (sender instanceof Player && Residence.getInstance().getEconomyManager().add(sender.getName(), amount) || !(sender instanceof Player)) {
+	    this.subtract(amount);
+	    Residence.getInstance().msg(sender, lm.Bank_Withdraw, String.format("%d", amount));
+	}
+    }
+
+    public void deposit(CommandSender sender, int amount, boolean resadmin) {
+	if (!(sender instanceof Player))
+	    return;
+	Player player = (Player) sender;
+	if (!Residence.getInstance().getConfigManager().enableEconomy()) {
+	    Residence.getInstance().msg(sender, lm.Economy_MarketDisabled);
+	}
+	if (!resadmin && !res.getPermissions().playerHas(player, Flags.bank, FlagCombo.OnlyTrue)) {
+	    Residence.getInstance().msg(sender, lm.Bank_NoAccess);
+	    return;
+	}
+	if (sender instanceof Player && !Residence.getInstance().getEconomyManager().canAfford(sender.getName(), amount)) {
+	    Residence.getInstance().msg(sender, lm.Economy_NotEnoughMoney);
+	    return;
+	}
+	if (sender instanceof Player && Residence.getInstance().getEconomyManager().subtract(sender.getName(), amount) || !(sender instanceof Player)) {
+	    this.add(amount);
+	    Residence.getInstance().msg(sender, lm.Bank_Deposit, String.format("%d", amount));
+	}
+    }
+}
---
+++ src/main/java/com/bekvon/bukkit/residence/utils/TabComplete.java
@@ -0,0 +1,27 @@
+package com.bekvon.bukkit.residence.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
+
+import com.bekvon.bukkit.residence.Residence;
+
+public class TabComplete implements TabCompleter {
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+	List<String> completionList = new ArrayList<>();
+	Set<String> Commands = Residence.getInstance().getHelpPages().getSubCommands(sender, args);
+
+	String PartOfCommand = args[args.length - 1];
+	StringUtil.copyPartialMatches(PartOfCommand, Commands, completionList);
+	Collections.sort(completionList);
+	return completionList;
+    }
+}
